4fb83e9773fb2be37bee3299749cbbfe812cdefe
shakil <shakilkhanhu@gmail.com>
2023-07-08 17:31:37 +0600

    homescreen

--- lib/utils/main_drawer.dart
+++ lib/utils/main_drawer.dart
@@ -14,18 +14,14 @@ class _MainDrawerState extends State<MainDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: Colors.transparent,
       child: ListView(
         children: [
-     
           ListTile(
             onTap: () async {
-
-
               Navigator.pop(context);
-
             },
-            leading: const Icon(Icons.logout),
-            title: const Text('LOGOUT'),
+            leading: const Icon(Icons.cancel_sharp),
           ),
         ],
       ),
