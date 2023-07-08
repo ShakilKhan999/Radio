0d14cbfda1f42f18a2e6667b6c55522a6b8f14db
Md. Shamim Raihan <md.shamimraihan@Mds-MacBook-Pro.local>
2023-07-08 17:32:17 +0600

    initial commit

---
+++ lib/screens/fund_raising.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FundRaising extends StatelessWidget {
+  const FundRaising({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
