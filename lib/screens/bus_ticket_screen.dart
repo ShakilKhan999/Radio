b5cd690bd97c7b9c75644efb87f46f1218e12b5c
shami <shamimraihan91@gmail.com>
2023-10-14 18:08:15 +0600

    s

--- lib/screens/bus_ticket_screen.dart
+++ lib/screens/bus_ticket_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:relaks_media/controller/otherservice_controller.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+
 
 import '../controller/home_controller.dart';
 import '../utils/glass_box.dart';
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,11 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  audioplayers_windows
-  file_selector_windows
-  firebase_core
-  geolocator_windows
-  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
